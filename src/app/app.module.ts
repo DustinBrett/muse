8ff5311259ef36ec80506df8515d825405c88211
Dustin Brett <dustinbrett@gmail.com>
2019-04-26 22:01:34 -0700

    Enable Ivy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,19 @@ import { DesktopIconComponent } from './components/desktop/icon/icon.component';
 import { TaskbarComponent } from './components/taskbar/taskbar.component';
 import { TaskbarIconComponent } from './components/taskbar/icon/icon.component';
 import { ClockComponent } from './components/taskbar/clock/clock.component';
+
 import { TooltipDirective } from './directives/tooltip.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+
     DesktopComponent,
     DesktopIconComponent,
     TaskbarComponent,
     TaskbarIconComponent,
     ClockComponent,
+
     TooltipDirective
   ],
   imports: [
