1493471e83be609a520ec407c9963398dfb6af3f
Dustin Brett <dustinbrett@gmail.com>
2019-04-27 12:27:35 -0700

    Clock improvements

--- src/app/components/taskbar/clock/clock.component.ts
+++ src/app/components/taskbar/clock/clock.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 const TIMEZONE_OFFSET = -7; // Pacific Daylight Time
 const MINUTE_MS = 60000;
@@ -9,16 +9,11 @@ const HOUR_MS = MINUTE_MS * 60;
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent implements AfterViewInit {
-  @ViewChild('time') time: ElementRef;
+export class ClockComponent {
+  public time = this.getTime();
 
-  ngAfterViewInit() {
-    this.setTime();
-    setInterval(this.setTime.bind(this), 1000);
-  }
-
-  padNumber(n: number): string {
-    return n.toString().padStart(2, '0');
+  constructor() {
+    setInterval(this.updateTime.bind(this), 1000);
   }
 
   getUTC(): number {
@@ -27,14 +22,23 @@ export class ClockComponent implements AfterViewInit {
     return now.getTime() + (now.getTimezoneOffset() * MINUTE_MS);
   }
 
-  setTime(): void {
+  getTime(): string {
     const now = new Date(this.getUTC() + (HOUR_MS * TIMEZONE_OFFSET));
     const hour24 = now.getHours();
-    const morning = hour24 <= 12;
-    const hour = this.padNumber(morning ? hour24 : hour24 - 12);
+    const morning = hour24 < 12;
+    const noon = hour24 === 12;
+    const hour = this.padNumber(morning || noon ? hour24 : hour24 - 12);
     const minute = this.padNumber(now.getMinutes());
     const second = this.padNumber(now.getSeconds());
 
-    this.time.nativeElement.innerHTML = `${hour}:${minute}:${second} ${morning ? 'AM' : 'PM'}`;
+    return `${hour}:${minute}:${second} ${morning ? 'AM' : 'PM'}`;
+  }
+
+  padNumber(num: number): string {
+    return num.toString().padStart(2, '0');
+  }
+
+  updateTime() {
+    this.time = this.getTime();
   }
 }
