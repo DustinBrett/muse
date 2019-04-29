74776a1490c44b17bd251206ab1e4cc0c0b949b0
Dustin Brett <dustinbrett@gmail.com>
2019-04-28 17:21:53 -0700

    Adjustments

--- frontend/src/app/components/desktop/desktop.component.ts
+++ frontend/src/app/components/desktop/desktop.component.ts
@@ -7,5 +7,33 @@ import { DESKTOP_ICONS } from '@core/config';
   styleUrls: ['./desktop.component.scss']
 })
 export class DesktopComponent {
+  private TOP_MARGIN = 5;
+  private ICON_SIZE = 102;
+  private TASKBAR_HEIGHT = 30;
+
+  public gridTemplateRows: string;
   public icons = DESKTOP_ICONS;
+
+  constructor() {
+    this.setGridTemplateRows();
+  }
+
+  onResize(event): void {
+    this.setGridTemplateRows();
+  }
+
+  getIconsPerColumn(innerHeight: number): number {
+    return Math.floor((innerHeight - (this.TOP_MARGIN + this.TASKBAR_HEIGHT)) / this.ICON_SIZE);
+  }
+
+  getGridTemplateRows(rowCount: number, returnString = ''): string {
+    return rowCount === 0
+      ? returnString
+      : this.getGridTemplateRows(rowCount - 1, returnString + `${this.ICON_SIZE}px `)
+    ;
+  }
+
+  setGridTemplateRows(): void {
+    this.gridTemplateRows = this.getGridTemplateRows(this.getIconsPerColumn(window.innerHeight));
+  }
 }
