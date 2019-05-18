ef3460ec5174f8a5e77cd576ad2ad0de0e74a002
Dustin Brett <dustinbrett@gmail.com>
2019-05-17 21:30:02 -0700

    Startup apps

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './services/app/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  private readonly startupIds: number[] = [
+    1 // Blog
+  ];
+
+  constructor(
+    private readonly appService: AppService
+  ) { }
+
+  ngOnInit() {
+    this.startupIds.forEach(id => {
+      this.appService.activate(id);
+    });
+  }
+}
