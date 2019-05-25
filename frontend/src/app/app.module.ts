42760c85f6c6417bc318c2f626b5eab70918750e
Dustin Brett <dustinbrett@gmail.com>
2019-05-24 21:52:21 -0700

    Remove BG for now

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SvgComponent } from './components/svg/svg.component';
 import { SvgDefinitionsComponent } from './components/svg/definitions/definitions.component';
 
-import { BackgroundComponent } from './components/desktop/background/background.component';
 import { DesktopComponent } from './components/desktop/desktop.component';
 import { DesktopIconComponent } from './components/desktop/icon/icon.component';
+
 import { TaskbarComponent } from './components/taskbar/taskbar.component';
 import { TaskbarIconComponent } from './components/taskbar/icon/icon.component';
 import { TaskbarEntryComponent } from './components/taskbar/entry/entry.component';
+
 import { ClockComponent } from './components/taskbar/clock/clock.component';
+
 import { WindowsComponent } from './components/windows/windows.component';
 import { WindowComponent } from './components/windows/window/window.component';
 import { WindowComponentDirective } from './directives/window-component/window-component.directive';
@@ -30,13 +32,15 @@ import { AngularDraggableModule } from 'angular2-draggable';
     SvgComponent,
     SvgDefinitionsComponent,
 
-    BackgroundComponent,
     DesktopComponent,
     DesktopIconComponent,
+
     TaskbarComponent,
     TaskbarIconComponent,
     TaskbarEntryComponent,
+
     ClockComponent,
+
     WindowsComponent,
     WindowComponent,
     WindowComponentDirective,
